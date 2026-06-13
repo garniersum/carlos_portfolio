40c777788b911f621e17c2d78375722cbb13185c
Carlos <carlos.gomez.mobile@gmail.com>
2026-06-13 12:33:18 -0600

    feat(schemas): add about, brands, contact, experience and skills document schemas

--- backend_sanity/schemaTypes/index.ts
+++ backend_sanity/schemaTypes/index.ts
@@ -1,4 +1,10 @@
-import { testimonialsType } from "./testimonials"
 import { aboutType } from "./about"
+import { brandsType } from "./brands"
+import { contactType } from "./contact"
+import { experiencesType } from "./experiences"
+import { skillsType } from "./skills"
+import { testimonialsType } from "./testimonials"
+import { workExperienceType } from "./workExperience"
+import { worksType } from "./works"
 
-export const schemaTypes = [testimonialsType, aboutType]
+export const schemaTypes = [ aboutType, brandsType, contactType,experiencesType, skillsType, testimonialsType, workExperienceType, worksType]
