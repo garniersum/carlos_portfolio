6ed9001ee54169e061474b29f358deb2ecf9d4a6
Carlos <carlos.gomez.mobile@gmail.com>
2026-06-19 21:33:51 -0600

    change logo, favicon and title

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -25,10 +25,9 @@ import about02 from '../assets/about02.png';
 import about03 from '../assets/about03.png';
 import about04 from '../assets/about04.png';
 
-import profile from '../assets/profile.png';
 import profile2 from '../assets/profile2.png';
 import circle from '../assets/circle.svg';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.ico';
 
 import adidas from '../assets/adidas.png';
 import amazon from '../assets/amazon.png';
@@ -64,7 +63,6 @@ export default {
   about02,
   about03,
   about04,
-  profile,
   profile2,
   circle,
   logo,
